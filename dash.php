31fceb0093de21498c40bee2e17975f38a76435c
umuhozaalice <umuhozaalice50@gmail.com>
2022-05-28 00:21:58 +1300

    reformed pages

---
+++ dash.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap-5.0.2-dist/css/bootstrap.rtl.min.css"><link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Login </title>
+</head>
+<body class="mr-5 ml-5">
+<div class="row pt1 pb-1" style="background:red; color:#fff;font-weight:600"><center>Assignment</center> </div>
+<div class="row pt-2 " >
+    <div class="col-md-8">
+        <div class="">
+        <div class="position-relative" style="height:25vh; background: url(Capture1.png); background-size: cover; background-repeat: no-repeat">
+        <span class="position-absolute bottom-0 start-0 ml-2">
+             <img class="img pt-5 " src="Picture1.png" alt="" width="150px">
+        </span>
+        </div>
+       
+        <div class="mt-5">
+             <hr>
+            <div class="col-sm-4 ">
+           
+                <h6><center>Headline</center></h6>
+            </div>
+            <span>Lorem ipsum dolor sit amet</span>
+        </div>
+        <div class="row ">
+        <a href="" class="btn border-danger border-top-0 border-right-0 border-left-0 bg-primary col-sm-12 mt-1" style="color:white; font-size:20px ;text-align:left; ">Change pasword</a>
+        </div>
+        <div class="container mt-2 mb-5 d-flex justify-content-center">
+            
+                <div class="card-body mr-5 ml-5">
+                        <div class="row d-flex flex-row">
+                        <div class="col-sm-3"><label for="">Current password</b></label></div>
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <input class="form-control" type="password" placeholder="password" name="password"> </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                        <div class="col-sm-3"><label for="">New password</b></label></div>
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <input class="form-control" type="password" placeholder="password" name="password"> </div>
+                            </div>
+                        </div>
+                        <div> <input type="checkbox" class="terms mt-3" style="color:skyblue;"><span>Lorem ipsum dolor sit amet</span> </center></div>
+                        <div class="row">
+                            <div class="col-sm-6">
+
+                            </div>
+                        <div class="mt-2 mb-1 col-sm-5"> <button class="btn btn-primary btn-block confirm-button">Set new password</button></div> 
+
+                        </div>
+                </div> 
+             
+        </div>
+    </div>
+</div>
+
+    <div class="col-md-4 " >
+        <ul style="list-style:none; text-decoration:none">
+            <li><a href="#" class="btn border-primary bg-white col-sm-12" style="color:skyblue; font-size:20px">ACCOUNT SETTING </a></li>
+            <li><a href="#" class="btn border-primary bg-white col-sm-12">Edit profile</a></li>
+            <li><a href="#" class="btn border-primary bg-white col-sm-12">Change password</a></li>
+            <li><a href="#" class="btn border-primary bg-white col-sm-12">Sign in activities</a></li>
+            <li><a href="#" class="btn border-primary bg-white col-sm-12">Session timeout</a></li>
+            <li><a href="#" class="btn border-primary bg-white col-sm-12">Memolialization </a></li>
+            <li><a href="#" class="btn border-primary bg-white col-sm-12">Jobs feeds contents</a></li>
+            <li><a href="#" class="btn border-primary bg-white col-sm-12">Email and sms notifications</a></li>
+            <li><a href="#" class="btn border-primary bg-white col-sm-12">Push notification </a></li>
+            <li><a href="#" class="btn border-primary bg-white col-sm-12">Delete account</a></li>
+        </ul>
+    </div>
+</div>
+<div class="row pt1 pb-1 bg-info" style="color:#fff;">
+<div class="col-md-1">About us</div>
+<div class="col-md-1">Advertisement</div>
+<div class="col-md-6"><center>Kigali, Rwanda</center></div>
+<div class="col-md-2">How search work</div>
+<div class="col-md-1">Privacy</div>
+</div>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap-5.0.2-dist/css/bootstrap.rtl.min.css">
+    <title>index page</title>
+    <style>
+        .form-group {
+            padding:2px;
+        }
+    </style>
+</head>
+
+<body class="mr-3 ml-3">
+<div class="row pb-2 bg-danger" style="color:#fff"><center>Assignment</center> </div>
+<div class="container mt-4 mb-4 d-flex justify-content-center">
+    <div class="card px-1 py-4 pt-5 pb-4 mt-4 mb-5" style="border:1px solid grey">
+        <div class="card-body">
+        <h3 class="card-title mb-3 pb-3"><center><b>Welcome back!</b></center></h3>
+            <h6 class="card-title mb-3 pb-3"><center>Sign In</center></h6>
+           <div class="row">
+                <div class="col-sm-12">
+                    <div class="form-group">
+                        <input class="form-control" type="text" placeholder="E-mail&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" name="email"> </div>
+                </div>
+            </div>
+            
+            <div class=" d-flex flex-column text-center px-5 mt-3 mb-1 mr-3 ml-3"> <button class="btn rounded-pill bg-info btn-block confirm-button">Next</button></div> <hr>
+            <div class=" d-flex flex-column text-center px-5 mt-3 mb-1"><button class="btn rounded-pill btn-danger btn-block confirm-button">&nbsp;&nbsp;&nbsp;&nbsp;Continue with google&nbsp;&nbsp;&nbsp;&nbsp;</button></div> 
+        <div> <a href="#" class="terms mt-3" style="color:skyblue;"><b>Create account</b></a> </div>
+        </div>
+    </div>
+</div>
+<div class="row pb-2 bg-info" style=" color:#fff;font-weight:600">
+<div class="col-md-1">About us</div>
+<div class="col-md-1">Advertisement</div>
+<div class="col-md-6"><center>Kigali, Rwanda</center></div>
+<div class="col-md-2">How search work</div>
+<div class="col-md-1">Privacy</div>
+</div>
+</body>
+</html>
